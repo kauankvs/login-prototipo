fbaec55bfb082d36abbf5d0e0affaa533197f69e
Kauan Vieira <kauanvieirakvs22@gmail.com>
2022-12-04 19:04:33 -0300

    comecinho

---
+++ Context/LoginContext.cs
@@ -0,0 +1,17 @@
+﻿using Login.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Login.Context
+{
+    public class LoginContext : DbContext
+    {
+        public LoginContext(DbContextOptions<LoginContext> options) : base(options) 
+        { 
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        { 
+            builder.Entity<Usuario>().ToTable("Usuario");
+        }
+    }
+}
