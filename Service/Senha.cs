1e04f17eaaf5c49484cb131584476668a50ad446
Kauan Vieira <kauanvieirakvs22@gmail.com>
2022-12-06 23:38:16 -0300

    service com registro de usuário

--- Service/Senha.cs
+++ Service/Senha.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography;
 using System.Text;
 
-namespace Login.Senha
+namespace Login.Service
 {
     public class Senha
     {
@@ -14,29 +14,36 @@ namespace Login.Senha
             _context = context;
         }
 
-        public void CriarSenhaComHashESalt(string senha, out byte[] senhaHash, out byte[] senhaSalt) 
-        { 
-            using (var hmac = new HMACSHA512()) 
+        public void CriarSenhaComHashESalt(string senha, out byte[] senhaHash, out byte[] senhaSalt)
+        {
+            using (var hmac = new HMACSHA512())
             {
                 senhaSalt = hmac.Key;
                 senhaHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(senha));
             }
         }
 
-        public async Task<bool> ChecarSeSenhaECorreta(string senha, string email) 
-        { 
+        public async Task<bool> ChecarSeSenhaECorretaAsync(string senha, string email)
+        {
             Usuario usuario = await _context.Usuarios.AsNoTracking().FirstOrDefaultAsync(user => user.Email.Equals(email));
             bool senhaCorreta = VerificarSenhaHash(senha, usuario.SenhaHash, usuario.SenhaSalt);
             return senhaCorreta;
         }
 
-        public bool VerificarSenhaHash(string senha, byte[] senhaHash, byte[] senhaSalt) 
+        public bool VerificarSenhaHash(string senha, byte[] senhaHash, byte[] senhaSalt)
         {
-            using(var hmac = new HMACSHA512(senhaSalt)) 
+            using (var hmac = new HMACSHA512(senhaSalt))
             {
                 var hashComputado = hmac.ComputeHash(Encoding.UTF8.GetBytes(senha));
                 return hashComputado.Equals(senhaHash);
             }
+        }
+
+        public async Task<bool> ChecarQueUsuarioExisteAsync(string email) 
+        {
+            Usuario usuario = null;
+            usuario = await _context.Usuarios.AsNoTracking().FirstOrDefaultAsync(user => user.Email == email);
+            return usuario != null;
         }
     }
 }
---
+++ Service/UsuarioService.cs
@@ -0,0 +1,45 @@
+﻿using Login.Context;
+using Login.DTOs;
+using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Service
+{
+    public class UsuarioService
+    {
+        private readonly LoginContext _context;
+        private readonly Senha _senha;
+        public UsuarioService(LoginContext context, Senha senha) 
+        {
+            _context = context;
+            _senha = senha;
+        }
+
+        public async Task<ActionResult<Usuario>> Registrar(UsuarioDTO usuarioDTO) 
+        {
+            byte[] senhaHash;
+            byte[] senhaSalt;
+            bool usuarioExiste = await _senha.ChecarQueUsuarioExisteAsync(usuarioDTO.Email);
+            if (usuarioExiste.Equals(true))
+            {
+                return new ConflictResult();
+            }
+
+            _senha.CriarSenhaComHashESalt(usuarioDTO.Senha, out senhaHash, out senhaSalt);
+            Usuario usuario = new Usuario()
+            {
+                Nome = usuarioDTO.Nome,
+                Sobrenome = usuarioDTO.Sobrenome,
+                Email = usuarioDTO.Email,
+                SenhaHash = senhaHash,
+                SenhaSalt = senhaSalt,
+                DataDeCriacao = DateTime.Now,
+                Papel = Papel.Cliente
+            };
+            await _context.Usuarios.AddAsync(usuario);
+            await _context.SaveChangesAsync();
+            return new CreatedResult("local", usuario);
+        }
+
+    }
+}
